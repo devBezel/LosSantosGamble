a3f5eb95d4acef3333fbfb6d6a6242feb9e3bc46
Bezel <dawids14@onet.pl>
2020-05-01 01:29:07 +0200

    migrated discord bot to another repository

--- lsg-discordbot/src/Config.ts
+++
@@ -1,8 +0,0 @@
-export const token: string = 'NTgzMDAxMDE2MTI3OTc5NTMw.XiyUgA.JqnED2r9wHBL3LIyNJcymb4twEE';
-export let owners: string[] = [];
-export const prefix: string = '!';
-export const databaseName: string = 'lsg_dicord';
-
-export const trelloApiKey: string = '0d62ed37fd938f7c0663cdb46dac5dbe';
-export const trelloOAuthToken: string = '39131d2a0e9e7a93848a11f0d355d023e644f6f045db79857d91722ae9609453';
-export const trelloBoardId: string = '5df0aa48a70c880854b83cdc';
--- lsg-discordbot/src/Models/TrelloListModel.ts
+++
@@ -1,9 +0,0 @@
-export interface TrelloListModel {
-    id: string;
-    name: string;
-    closed: boolean;
-    pos: number;
-    softLimit: any;
-    idBoard: string;
-    subscribed: boolean;
-}
